59ab2485f41bbe383171f67c2e180639bdefba57
jslee <dlwhdtjd012@kakao.com>
2022-10-16 18:30:53 +0900

    ADD recruit views.py - CBV로 채용공고, 회사, 지원자, 지원내역 API 작성

--- webapp/recruit/views.py
+++ webapp/recruit/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from recruit.models import Applicant, Company, JobPosting, ApplicationHistory
+from recruit.serializers import ApplicantSerializer, CompanySerializer, JobPostingSerializer, \
+    ApplicationHistorySerializer
+
+
+def index(request):
+    pass
+
+
+class ApplicantViewSet(viewsets.ModelViewSet):
+    """
+
+    """
+    queryset = Applicant.objects.all().order_by('name')
+    serializer_class = ApplicantSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class JobPostingViewSet(viewsets.ModelViewSet):
+    queryset = JobPosting.objects.all().order_by('-id')
+    serializer_class = JobPostingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all().order_by('name')
+    serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ApplicationHistoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ApplicationHistory.objects.all().order_by('-id')
+    serializer_class = ApplicationHistorySerializer
+    permission_classes = [permissions.IsAuthenticated]
