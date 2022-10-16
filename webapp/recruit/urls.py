1bb24fba89a320450be6fb9d2c934b2f10493ab5
jslee <dlwhdtjd012@kakao.com>
2022-10-16 18:31:17 +0900

    ADD recruit urls.py - 채용공고, 회사, 지원자, 지원내역 url 매핑

--- webapp/recruit/urls.py
+++ webapp/recruit/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
-from . import views
 from rest_framework import routers
 
+from . import views
+
 router = routers.DefaultRouter()
-router.register('applicant',views.ApplicantViewSet)
+router.register('applicant', views.ApplicantViewSet)
+router.register('job-posting', views.JobPostingViewSet)
+router.register('company', views.CompanyViewSet)
+router.register('application-history', views.ApplicationHistoryViewSet)
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('', include(router.urls), name='applicant')
+    path('', include(router.urls), name='recruit')
 ]
