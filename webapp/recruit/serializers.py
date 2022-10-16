f2abbe5d4c6b5865f2aeee57ea287575415846c3
jslee <dlwhdtjd012@kakao.com>
2022-10-16 18:28:17 +0900

    INIT & ADD recruit serializers.py - 채용공고, 회사, 지원자, 지원내역 serializers 생성

---
+++ webapp/recruit/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from . import models
+from .models import Company
+
+
+class ApplicantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Applicant
+        fields = ['cmopany', 'name', 'email', 'skill']
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    job_postings = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = models.Company
+        read_only_fields = ('id',)
+        fields = ['name', 'email', 'country', 'region', 'job_postings']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Company.objects.all(),
+                fields=['name', 'email']
+            )
+        ]
+
+
+class JobPostingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.JobPosting
+        read_only_fields = ('id',)
+        fields = ['company', 'title', 'content', 'skill', 'recruit_compensation', 'recruit_position']
+
+
+class ApplicationHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        read_only_fields = '__all__'
+        model = models.ApplicationHistory
+        fields = '__all__'
