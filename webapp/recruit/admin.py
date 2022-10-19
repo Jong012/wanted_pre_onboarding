9074207a65d8b8a7c68fcbddd0668c5df9ec0635
jslee <dlwhdtjd012@kakao.com>
2022-10-19 16:18:26 +0900

    UPDATE admin.py - 지원자, 회사, 채용공고, 지원내역을 추가

--- webapp/recruit/admin.py
+++ webapp/recruit/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+@admin.register(models.Applicant)
+class AdminApplicant(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.JobPosting)
+class AdminJobPosting(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Company)
+class AdminCompany(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ApplicationHistory)
+class ApplicationHistory(admin.ModelAdmin):
+    pass
