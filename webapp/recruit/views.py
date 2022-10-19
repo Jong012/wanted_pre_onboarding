145dfe28fc06b5dbd96174f44596da95f7138ade
jslee <dlwhdtjd012@kakao.com>
2022-10-19 16:16:40 +0900

    UPDATE views.py - viewset comment, django-filter 추가 및 permissions, readonly viewset 변경
    1. viewset api comment 추가
    2. job-posting django-filter 추가
    3. ApplicantViewSet, CompanyViewSet ReadOnlyModelViewSet 으로 변경, ApplicationHistroy 는 ReadOnly 에서 ModelViewSet 으로 변경
    4. 모든 viewset 의 접근 권한을 로그인 안해도 되는 걸로 수정.

--- webapp/recruit/views.py
+++ webapp/recruit/views.py
@@ -1,38 +1,51 @@
 # Create your views here.
 from rest_framework import permissions
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from recruit.models import Applicant, Company, JobPosting, ApplicationHistory
-from recruit.serializers import ApplicantSerializer, CompanySerializer, JobPostingSerializer, \
-    ApplicationHistorySerializer
+from recruit.serializers import ApplicantSerializer, CompanySerializer, \
+    ApplicationHistorySerializer, JobPostingDetailSerializer, JobPostingListSerializer
 
 
-def index(request):
-    pass
-
-
-class ApplicantViewSet(viewsets.ModelViewSet):
+class ApplicantViewSet(viewsets.ReadOnlyModelViewSet):
     """
-
+    지원자 view
     """
     queryset = Applicant.objects.all().order_by('name')
     serializer_class = ApplicantSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 
 class JobPostingViewSet(viewsets.ModelViewSet):
+    """
+    채용공고
+    """
     queryset = JobPosting.objects.all().order_by('-id')
-    serializer_class = JobPostingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [SearchFilter]
+    search_fields = ['company__name', 'content',
+                     'recruit_compensation', 'recruit_position', 'skill', 'title']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return JobPostingListSerializer
+        return JobPostingDetailSerializer
 
 
-class CompanyViewSet(viewsets.ModelViewSet):
+class CompanyViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    회사
+    """
     queryset = Company.objects.all().order_by('name')
     serializer_class = CompanySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 
-class ApplicationHistoryViewSet(viewsets.ReadOnlyModelViewSet):
+class ApplicationHistoryViewSet(viewsets.ModelViewSet):
+    """
+    지원자
+    """
     queryset = ApplicationHistory.objects.all().order_by('-id')
     serializer_class = ApplicationHistorySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
